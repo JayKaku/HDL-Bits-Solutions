41391389ad41b054f224e207005fd8d63faddcad
JayKaku <jay8kaku@gmail.com>
2023-06-02 12:24:49 +0530

    Day 13

---
+++ Circuits/Sequential Logic/Latches and Flip-Flops/15 - Detect an edge.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    
+    reg [7:0]inter;
+    
+    always @ (posedge clk) begin
+        inter <= in;
+        pedge <= (~inter) & in;
+    end
+
+endmodule
