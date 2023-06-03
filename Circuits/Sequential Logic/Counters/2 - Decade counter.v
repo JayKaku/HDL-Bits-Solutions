53ffed654674e1fa82e42473ad60561f6d627890
JayKaku <jay8kaku@gmail.com>
2023-06-03 14:42:59 +0530

    Day 14 (100 complete on HDL bits🥳)

---
+++ Circuits/Sequential Logic/Counters/2 - Decade counter.v
@@ -0,0 +1,19 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+       
+        if(reset == 1'b1) begin
+           q <= 0;
+        end
+        else if(q == 4'b1001) begin 
+             q <= 0;
+        end
+        else begin 
+           q <= q + 1; 
+        end
+    end
+
+endmodule
