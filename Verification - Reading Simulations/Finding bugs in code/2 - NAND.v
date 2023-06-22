f28d10b1df1337e5122eb976726d99624dababcc
JayKaku <jay8kaku@gmail.com>
2023-06-22 13:55:15 +0530

    Day 28 nand correction

--- Verification - Reading Simulations/Finding bugs in code/2 - NAND.v
+++ Verification - Reading Simulations/Finding bugs in code/2 - NAND.v
@@ -2,7 +2,7 @@ module top_module (input a, input b, input c, output out);//
 	
     wire w1;
     
-    andgate inst1 ( out, a, b, c, c, c );
+    andgate inst1 ( w1, a, b, c, c, c );
     
     assign out = ~w1;
 
